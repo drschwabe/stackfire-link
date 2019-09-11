af2bdbf56f5727e8131f378f4e964b3a29523755
Derrick Schwabe <admin@nerdbusiness.com>
2019-09-11 11:00:10 -0700

    Accommodate for Electron and convert any URL enncoded spaces to normal spaces

--- listen.js
+++ listen.js
@@ -1,13 +1,28 @@
 const $ = require('jquery')
 const _s = require('underscore.string')
 
+if(window.isElectron) {
+  const shell = window.require('electron').shell //TODO: make compatible with non Electron
+}
+
 module.exports = (stack, basename) => {
   if(basename) stack.state.link_basename = basename
   stack.on('init', () => {
-    var fireLink = (pathname, event) => {
-      console.log('fire link..')
+    var fireLink = (link, event) => {
+
+      //if web link, just return so default handling
+      if(window.isElectron) {
+        if( _s.include(link.href, 'http')) {
+          event.preventDefault()
+          return shell.openExternal(link.href)
+        }
+      }
+
+      let pathname = decodeURIComponent(link.pathname) //< convert any URL enncoded spaces to normal spaces
+      //if(_s.include(link'http'))
+
       if(pathname.search(basename) > -1) {
-        var strippedLink = _s.strRight(pathname, basename + '/')
+        let strippedLink = _s.strRight(pathname, basename + '/')
         if(event) {
           event.stopPropagation()
           event.preventDefault()
@@ -21,7 +36,7 @@ module.exports = (stack, basename) => {
     $(document).on('click', (event) => {
       if($(event.target).is('a, a *')) {
         var link = $(event.target).closest('a')[0]
-        return fireLink(link.pathname, event)
+        return fireLink(link, event)
       }
     })
   })
