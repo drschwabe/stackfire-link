edfed996893e897f15124e5415d7494956225475
Derrick Schwabe <admin@nerdbusiness.com>
2019-09-12 12:11:21 -0700

    Remove the need to invoke this module's functionality with an explicit 'init' fire.

--- listen.js
+++ listen.js
@@ -7,37 +7,35 @@ if(window.isElectron) {
 
 module.exports = (stack, basename) => {
   if(basename) stack.state.link_basename = basename
-  stack.on('init', () => {
-    var fireLink = (link, event) => {
+  var fireLink = (link, event) => {
 
-      //if web link, just return so default handling
-      if(window.isElectron) {
-        if( _s.include(link.href, 'http')) {
-          event.preventDefault()
-          return shell.openExternal(link.href)
-        }
-      }
-
-      let pathname = decodeURIComponent(link.pathname) //< convert any URL enncoded spaces to normal spaces
-      //if(_s.include(link'http'))
-
-      if(pathname.search(basename) > -1) {
-        let strippedLink = _s.strRight(pathname, basename + '/')
-        if(event) {
-          event.stopPropagation()
-          event.preventDefault()
-          state.event = event
-        }
-        stack.fire( strippedLink , () => {
-          state.event = null
-        })
+    //if web link, just return so default handling
+    if(window.isElectron) {
+      if( _s.include(link.href, 'http')) {
+        event.preventDefault()
+        return shell.openExternal(link.href)
       }
     }
-    $(document).on('click', (event) => {
-      if($(event.target).is('a, a *')) {
-        var link = $(event.target).closest('a')[0]
-        return fireLink(link, event)
+
+    let pathname = decodeURIComponent(link.pathname) //< convert any URL enncoded spaces to normal spaces
+    //if(_s.include(link'http'))
+
+    if(pathname.search(basename) > -1) {
+      let strippedLink = _s.strRight(pathname, basename + '/')
+      if(event) {
+        event.stopPropagation()
+        event.preventDefault()
+        state.event = event
       }
-    })
+      stack.fire( strippedLink , () => {
+        state.event = null
+      })
+    }
+  }
+  $(document).on('click', (event) => {
+    if($(event.target).is('a, a *')) {
+      var link = $(event.target).closest('a')[0]
+      return fireLink(link, event)
+    }
   })
 }
